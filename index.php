143c52914d5cc8a7a5a9cd652b3656cc3c2db50b
root <root@ip-10-204-238-230.ec2.internal>
2013-04-13 21:41:22 +0000

    center main page

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 include('user_agent.php'); // Redirect to mobile site
 ?>
 
+<center>
 <h1>Under Construction</h1>
 
 In the meanwhile, please visit out <a href="/m/index.html">mobile site</a>!
-
+<br />
 <img src="img.png" />
-
+</center>
