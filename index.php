674e1662a6845ca569234f21a59937e295109fcb
Shauvik Roy Choudhary <shauvik@gmail.com>
2013-04-13 17:31:23 -0400

    Adding default desktop site

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include('user_agent.php'); // Redirect to mobile site
+?>
+
+<h1>Under Construction</h1>
+
+In the meanwhile, please visit out <a href="/m/index.html">mobile site</a>!
+
+<img src="img.png" />
+
