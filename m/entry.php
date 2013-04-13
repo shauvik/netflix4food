1260494091ee04138c6866ed05c2223061e65e9e
gewthen <likeehad>
2013-04-13 12:04:46 -0400

    entry

---
+++ m/entry.php
@@ -0,0 +1,26 @@
+<?php
+//include_once("common.php");
+
+print_r($_REQUEST);
+
+//function saveSeller()
+//{
+//
+//    $name = $_POST['name'];
+//    $phone = $_POST['phone'];
+//    $email = $_POST['email'];
+//    $hours = $_POST['hours'];
+//    $location = $_POST['location'];
+//    $description = $_POST['description'];
+//
+//    $sql = "INSERT into people (name, phone, email, hours, location, description) VALUES(? , ?, ? ,?, ?, ?)";
+//    $mysqli = get_database_connection();
+//    $stmt = $mysqli->prepare($sql);
+//    $stmt->bind_param($name, $phone, $email, $hours, $location, $description);
+//    $stmt->execute();
+//}
+//
+//function getSeller(){
+//
+
+//}
