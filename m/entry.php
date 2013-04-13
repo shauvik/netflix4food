77fc874c56b01546a6f3d4027e591bc55db4454e
gewthen <likeehad>
2013-04-13 15:59:23 -0400

    Added username.

--- m/entry.php
+++ m/entry.php
@@ -15,6 +15,7 @@ function saveSeller()
    $rate = $_POST['rate'];
    $location = $_POST['location'];
    $description = $_POST['description'];
+   $userName = $_POST['userName'];
 
    // $sql = "INSERT into users (role, name, phone, email, deliver, rate, location, description) VALUES(?, ?, ?, ? ,?, ?, ?, ?)";
    // $mysqli = get_database_connection();
@@ -22,7 +23,7 @@ function saveSeller()
    // $stmt->bind_param($role, $name, $phone, $email, $deliver, $rate, $location, $description) or die("Bind error:".$mysqli->error);
    // $stmt->execute() or die("Mysql exec error:".$mysqli->error);	
 
-   $sql = "INSERT into users (role, name, phone, email, deliver, rate, location, description) VALUES('$role', '$name', '$phone', '$email' , $deliver, '$rate', '$location', '$description')";
+   $sql = "INSERT into users (userName, role, name, phone, email, deliver, rate, location, description) VALUES('$userName', ''$role', '$name', '$phone', '$email' , $deliver, '$rate', '$location', '$description')";
    $mysqli = get_database_connection();
    //echo "<br />Query:".$sql;
    $mysqli->query($sql) or die("Error in mysql query");
