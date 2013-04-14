60e72006d98e1c65dcb9ee31cb16adf82bd7ea7a
Chinedu Ezeamuzie <chinchin96@gmail.com>
2013-04-14 01:58:08 -0400

    added welcome message

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include('user_agent.php'); // Redirect to mobile site
 <center>
 <h1>Under Construction</h1>
 
-In the meanwhile, please visit out <a href="/m/index.php">mobile site</a>!
+Welcome to Fuber. In the meanwhile, please visit out <a href="/m/index.php">mobile site</a>!
 <br />
 <img src="img.png" />
 </center>
